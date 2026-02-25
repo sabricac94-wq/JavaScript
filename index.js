331433d10095be1537af807cec8ab8ca20872323
Sofia <sabricac94@gmail.com>
2026-02-24 18:57:28 -0600

    ordene mi codigo javascript y aprendí otros tipos de datos

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("hola mundo")
+console.log("hola mundo")
+let nombre = "Sofia"
+//nombre = "Ana"
+let primerNombre = "Ana"
+console.log(primerNombre + " " + nombre)
+let edad = 31
+let booleano = true 
