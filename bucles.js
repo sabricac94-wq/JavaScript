e7961d92757a9fc3364b88db17fc96d4de28130e
Sofia <sabricac94@gmail.com>
2026-03-10 18:59:08 -0600

    aprendí condicionales y bucles

---
+++ bucles.js
@@ -0,0 +1,30 @@
+//variable de inicializacion; condicion; suma, rests o multiplicacion
+/*
+for (let colima = 5; colima <= 10 ; colima++) { 
+    console.log(colima)
+    
+}
+//Imprime los numeros del 10 al 0
+for (let index = 10; index >= 0 ; index--) {
+    console.log(index)
+    
+}
+*/
+// imprime los numeros sumando de dos en dos
+/*for (let index = 0; index <= 20 ; index = index+2) {
+    console.log(index)
+    
+}*/
+
+for (let index = 0; index < 10 ; index++) {
+    if (index === 6) {
+        break
+    }
+    console.log(index)
+
+    
+}
+
+//un igual significa asignacion, o sea que le estamos dando un valor
+//== significa comparar el valor
+//=== significa comparar valor y tipo de dato
--- if-else.js
+++ if-else.js
@@ -5,3 +5,21 @@ if (edad >=18) {
     console.log("no puedes entrar eres menor de edad")
     
 }
+
+let variable1 = 1
+let variable2 = "1"
+console.log(typeof variable2)
+console.log(variable1 == variable2) //estamos comparando el valor 
+console.log(variable1 === variable2) //estamos comparando el valor y tipo de dato
+
+
+
+
+
+/* git status: verifico si mis archivos hn sido agregados a la zona de trabajo
+git add -A: agregar mis modificaciones a la zona de trabajo
+git commit -m: agregar comentarios
+git push origin *nombre de la rama*
+Ir a mi cuenta de github y hacer el pull request
+volver a visual studio y moverme a la rama master
+git pull origin master: descargar la fusión de mi cuenta de github a visual studio*/
